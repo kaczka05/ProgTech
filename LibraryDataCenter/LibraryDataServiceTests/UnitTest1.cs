205216afaf6a4808a75203d738f532fc6605ec53
Jakub Babaniuk <jbabaniuk@gmail.com>
2025-05-19 23:58:38 +0200

    guitest

---
+++ LibraryDataCenter/LibraryDataServiceTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibraryDataServiceTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DummyTest_ShouldAlwaysPass()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}

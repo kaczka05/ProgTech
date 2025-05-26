8a91bd4edc674661179c5a39912d040c6fe9f437
DragoDam <widstarasd1@gmail.com>
2025-05-26 20:30:11 +0200

    Data layer compatible with no database

--- LibraryDataCenter/LibraryDataCenter/LibraryDataRepository.cs
+++ LibraryDataCenter/LibraryDataCenter/LibraryDataRepository.cs
@@ -156,8 +156,6 @@ namespace LibraryDataLayer
             }
         }
 
-        // Pomocnicza konwersja Events -> IEvent
-
 
         private IEvent? EntryToObj(Events ev)
         {
@@ -189,93 +187,179 @@ namespace LibraryDataLayer
 
         public void AddUser(int userId, string firstName, string lastName)
         {
-            Users user = new Users
+            if (connected)
             {
-                UserId = userId,
-                FirstName = firstName,
-                LastName = lastName
-            };
-            _libraryDataContext.Users.InsertOnSubmit(user);
-            _libraryDataContext.SubmitChanges();
+                Users user = new Users
+                {
+                    UserId = userId,
+                    FirstName = firstName,
+                    LastName = lastName
+                };
+                _libraryDataContext.Users.InsertOnSubmit(user);
+                _libraryDataContext.SubmitChanges();
+            }
+            else
+            {
+                User user = new User(userId, firstName, lastName);
+                _libraryDataContextOff.AddUserAsync(user);
+            }
         }
 
 
         public void RemoveUserById(int id)
         {
-            Users user = _libraryDataContext.Users.Single(u => u.UserId == id);
-            _libraryDataContext.Users.DeleteOnSubmit(user);
-            _libraryDataContext.SubmitChanges();
+            if(connected)
+            {
+                Users user = _libraryDataContext.Users.Single(u => u.UserId == id);
+                _libraryDataContext.Users.DeleteOnSubmit(user);
+                _libraryDataContext.SubmitChanges();
+            }
+            else
+            {
+                User user = (User)_libraryDataContextOff.Users.Single(u => u.UserId == id);
+                _libraryDataContextOff.RemoveUserAsync(user);
+            }
+            
         }
 
    
         public IUser? GetUserById(int id)
         {
+            if(connected)
+            { 
             var user = (from u in _libraryDataContext.Users
                         where u.UserId == id
                         select u).FirstOrDefault();
             return EntryToObj(user);
+            }
+            else
+            {
+                var user = _libraryDataContextOff.Users
+                    .FirstOrDefault(u => u.UserId == id);
+                if (user == null)
+                {
+                    return null;
+                }
+                return new User(user.UserId, user.FirstName, user.LastName);
+            }
+
         }
 
      
         public IEnumerable<IUser> GetAllUsers()
         {
-            var users = from u in _libraryDataContext.Users
-                        select EntryToObj(u);
-            return users;
+            if (!connected)
+            {
+                return _libraryDataContextOff.Users
+                    .Select(u => new User(u.UserId, u.FirstName, u.LastName));
+            }
+            else
+            {
+                var users = from u in _libraryDataContext.Users
+                            select EntryToObj(u);
+                return users;
+            }
+           
         }
 
-  
+
         public void AddDatabaseEvent(int eventId, int employeeId, int stateId, bool addition)
         {
-            Events ev = new Events
+            if (connected)
             {
-                EventId = eventId,
-                Employee = employeeId,
-                State = stateId,
-                Addition = addition,
-                EventType = "Database"
-            };
-            _libraryDataContext.Events.InsertOnSubmit(ev);
-            _libraryDataContext.SubmitChanges();
+                Events ev = new Events
+                {
+                    EventId = eventId,
+                    Employee = employeeId,
+                    State = stateId,
+                    Addition = addition,
+                    EventType = "Database"
+                };
+                _libraryDataContext.Events.InsertOnSubmit(ev);
+                _libraryDataContext.SubmitChanges();
+            }
+            else
+            {
+                DatabaseEvent ev = new DatabaseEvent(eventId, GetUserById(employeeId), GetStateById(stateId), addition);
+                _libraryDataContextOff.AddDatabaseEventAsync(ev);
+            }
         }
 
-       
+
         public void AddUserEvent(int eventId, int employeeId, int stateId, int userId, bool borrowing)
         {
-            Events ev = new Events
+            if (connected)
             {
-                EventId = eventId,
-                Employee = employeeId,
-                State = stateId,
-                User = userId,
-                Borrowing = borrowing,
-                EventType = "User"
-            };
-            _libraryDataContext.Events.InsertOnSubmit(ev);
-            _libraryDataContext.SubmitChanges();
+                Events ev = new Events
+                {
+                    EventId = eventId,
+                    Employee = employeeId,
+                    State = stateId,
+                    User = userId,
+                    Borrowing = borrowing,
+                    EventType = "User"
+                };
+                _libraryDataContext.Events.InsertOnSubmit(ev);
+                _libraryDataContext.SubmitChanges();
+            }
+            else
+            {
+                UserEvent ev = new UserEvent(eventId, GetUserById(employeeId), GetStateById(stateId), GetUserById(userId), borrowing);
+                _libraryDataContextOff.AddUserEventAsync(ev);
+            }
         }
-
     
         public void RemoveEventById(int id)
         {
-            Events ev = _libraryDataContext.Events.Single(e => e.EventId == id);
-            _libraryDataContext.Events.DeleteOnSubmit(ev);
-            _libraryDataContext.SubmitChanges();
+            if (connected)
+            {
+                Events ev = _libraryDataContext.Events.Single(e => e.EventId == id);
+                _libraryDataContext.Events.DeleteOnSubmit(ev);
+                _libraryDataContext.SubmitChanges();
+            }
+            else
+            {
+                IEvent ev = _libraryDataContextOff.DatabaseEvents.Single(e => e.EventId == id);
+                _libraryDataContextOff.RemoveEventAsync(ev);
+            }
+
         }
 
         public IEvent? GetEventById(int id)
         {
-            var ev = (from e in _libraryDataContext.Events
-                      where e.EventId == id
-                      select e).FirstOrDefault();
-            return EntryToObj(ev);
+            if (!connected)
+            {
+                var ev = _libraryDataContextOff.DatabaseEvents
+                    .FirstOrDefault(e => e.EventId == id);
+                if (ev == null)
+                {
+                    return null;
+                }
+                return ev;
+            }
+            else
+            {
+                var ev = (from e in _libraryDataContext.Events
+                          where e.EventId == id
+                          select e).FirstOrDefault();
+                return EntryToObj(ev);
+            }
+            
         }
 
         public IEnumerable<IEvent> GetAllEvents()
         {
+            if (!connected)
+            {
+                return _libraryDataContextOff.DatabaseEvents
+                    .Select(e => EntryToObj(e));
+            }
+            else 
+            { 
             var events = from e in _libraryDataContext.Events
                          select EntryToObj(e);
             return events;
+            }
         }
       
         private IState? EntryToObj(States state)
@@ -294,37 +378,76 @@ namespace LibraryDataLayer
    
         public void AddState(int stateId, int nrOfBooks, int catalogId)
         {
-            States state = new States
+            if(connected)
             {
-                StateId = stateId,
-                NrOfBooks = nrOfBooks,
-                Catalog = catalogId
-            };
-            _libraryDataContext.States.InsertOnSubmit(state);
-            _libraryDataContext.SubmitChanges();
+                States state = new States
+                {
+                    StateId = stateId,
+                    NrOfBooks = nrOfBooks,
+                    Catalog = catalogId
+                };
+                _libraryDataContext.States.InsertOnSubmit(state);
+                _libraryDataContext.SubmitChanges();
+            }
+            else
+            {
+                State state = new State(stateId, nrOfBooks, GetCatalogById(catalogId));
+                _libraryDataContextOff.AddStateAsync(state);
+            }
+
         }
 
      
         public void RemoveStateByID(int id)
         {
-            States state = _libraryDataContext.States.Single(s => s.StateId == id);
-            _libraryDataContext.States.DeleteOnSubmit(state);
-            _libraryDataContext.SubmitChanges();
+            if (!connected)
+            {
+                State state = (State)_libraryDataContextOff.States.Single(s => s.StateId == id);
+                _libraryDataContextOff.RemoveStateAsync(state);
+                return;
+            }
+            else
+            {
+                States state = _libraryDataContext.States.Single(s => s.StateId == id);
+                _libraryDataContext.States.DeleteOnSubmit(state);
+                _libraryDataContext.SubmitChanges();
+            }
         }
 
         public IState? GetStateById(int id)
         {
-            var state = (from s in _libraryDataContext.States
-                         where s.StateId == id
-                         select s).FirstOrDefault();
-            return EntryToObj(state);
+            if (!connected)
+            {
+                var state = _libraryDataContextOff.States
+                    .FirstOrDefault(s => s.StateId == id);
+                if (state == null)
+                {
+                    return null;
+                }
+                return new State(state.StateId, state.NrOfBooks, GetCatalogById(state.CatalogId));
+            }
+            else
+            {
+                var state = (from s in _libraryDataContext.States
+                             where s.StateId == id
+                             select s).FirstOrDefault();
+                return EntryToObj(state);
+            }
         }
 
         public IEnumerable<IState> GetAllStates()
         {
-            var states = from s in _libraryDataContext.States
-                         select EntryToObj(s);
+            if (!connected)
+            {
+                return _libraryDataContextOff.States
+                    .Select(s => new State(s.StateId, s.NrOfBooks, GetCatalogById(s.CatalogId)));
+            }
+            else
+            { 
+                var states = from s in _libraryDataContext.States
+                             select EntryToObj(s);
             return states;
+            }
         }
 
 
@@ -333,24 +456,47 @@ namespace LibraryDataLayer
 
         public bool DoesCatalogExist(int id)
         {
-            /*if (_libraryDataContext == null) throw new InvalidOperationException("Data context is not initialized.");
-            return _libraryDataContext.Books.Any(c => c.catalogId == id);*/
-            return false;
+            if (connected)
+            {
+                return _libraryDataContext.Books.Any(c => c.catalogId == id);
+            }
+            else
+            {
+                return _libraryDataContextOff.Catalogs.Any(c => c.CatalogId == id);
+            }
         }
         public bool DoesUserExist(int id)
         {
-            if (_libraryDataContext == null) throw new InvalidOperationException("Data context is not initialized.");
-            return _libraryDataContext.Users.Any(u => u.UserId == id);
+            if(connected)
+            {
+                return _libraryDataContext.Users.Any(u => u.UserId == id);
+            }
+            else
+            {
+                return _libraryDataContextOff.Users.Any(u => u.UserId == id);
+            }
         }
         public bool DoesEventExist(int id)
         {
-            if (_libraryDataContext == null) throw new InvalidOperationException("Data context is not initialized.");
-            return _libraryDataContext.Events.Any(e => e.EventId == id);
+            if (connected)
+            {
+                return _libraryDataContext.Events.Any(e => e.EventId == id);
+            }
+            else
+            {
+                return _libraryDataContextOff.DatabaseEvents.Any(e => e.EventId == id);
+            }
         }
         public bool DoesStateExist(int id)
         {
-            if (_libraryDataContext == null) throw new InvalidOperationException("Data context is not initialized.");
-            return _libraryDataContext.States.Any(s => s.StateId == id);
+            if (connected)
+            {
+                return _libraryDataContext.States.Any(s => s.StateId == id);
+            }
+            else
+            {
+                return _libraryDataContextOff.States.Any(s => s.StateId == id);
+            }
         }
 
     }
